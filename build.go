65a38777d369ab9d13f44013127f751838700bc8
Tiago Posse <tiago.posse@hi.health>
2023-08-20 19:10:37 +0200

    use targetbuilder

--- build.go
+++ build.go
@@ -1,36 +1,34 @@
 package npm
 
 import (
-	"os/exec"
-
 	zen_targets "github.com/zen-io/zen-core/target"
 )
 
 type TypescriptBuildConfig struct {
-	Out string `mapstructure:"out"`
-	zen_targets.BuildFields
+	Name          string            `mapstructure:"name" zen:"yes" desc:"Name for the target"`
+	Description   string            `mapstructure:"desc" zen:"yes" desc:"Target description"`
+	Labels        []string          `mapstructure:"labels" zen:"yes" desc:"Labels to apply to the targets"`
+	Deps          []string          `mapstructure:"deps" zen:"yes" desc:"Build dependencies"`
+	PassEnv       []string          `mapstructure:"pass_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	PassSecretEnv []string          `mapstructure:"secret_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env           map[string]string `mapstructure:"env" zen:"yes" desc:"Key-Value map of static environment variables to be used"`
+	Tools         map[string]string `mapstructure:"tools" zen:"yes" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility    []string          `mapstructure:"visibility" zen:"yes" desc:"List of visibility for this target"`
+	Srcs          []string          `mapstructure:"srcs" desc:"Sources for the build"`
+	Out           string            `mapstructure:"out"`
 }
 
-func (tsc TypescriptBuildConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
-	return []*zen_targets.Target{
-		zen_targets.NewTarget(
-			tsc.Name,
-			zen_targets.WithSrcs(map[string][]string{"_srcs": tsc.Srcs}),
-			zen_targets.WithOuts([]string{tsc.Out}),
-			zen_targets.WithPassEnv(tsc.PassEnv),
-			zen_targets.WithEnvVars(tsc.Env),
-			zen_targets.WithVisibility(tsc.Visibility),
-			zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
-				Deps: tsc.Deps,
-				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					cmd := exec.Command("tsc", "-p", ".")
-					cmd.Dir = target.Cwd
-					cmd.Env = target.GetEnvironmentVariablesList()
-					cmd.Stdout = target
-					cmd.Stderr = target
-					return cmd.Run()
-				},
-			}),
-		),
-	}, nil
+func (tsc TypescriptBuildConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.TargetBuilder, error) {
+
+	tb := zen_targets.ToTarget(tsc)
+	tb.Srcs = map[string][]string{"_srcs": tsc.Srcs}
+	tb.Outs = []string{tsc.Out}
+
+	tb.Scripts["build"] = &zen_targets.TargetBuilderScript{
+		Deps: tsc.Deps,
+		Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			return target.Exec([]string{"tsc", "-p", "."}, "node build")
+		},
+	}
+	return []*zen_targets.TargetBuilder{tb}, nil
 }
--- ci.go
+++ ci.go
@@ -1,35 +1,33 @@
 package npm
 
 import (
-	"os/exec"
-
 	zen_targets "github.com/zen-io/zen-core/target"
 )
 
 type NpmCiConfig struct {
-	zen_targets.BuildFields
+	Name          string            `mapstructure:"name" zen:"yes" desc:"Name for the target"`
+	Description   string            `mapstructure:"desc" zen:"yes" desc:"Target description"`
+	Labels        []string          `mapstructure:"labels" zen:"yes" desc:"Labels to apply to the targets"` //
+	Deps          []string          `mapstructure:"deps" zen:"yes" desc:"Build dependencies"`
+	PassEnv       []string          `mapstructure:"pass_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	PassSecretEnv []string          `mapstructure:"secret_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env           map[string]string `mapstructure:"env" zen:"yes" desc:"Key-Value map of static environment variables to be used"`
+	Tools         map[string]string `mapstructure:"tools" zen:"yes" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility    []string          `mapstructure:"visibility" zen:"yes" desc:"List of visibility for this target"`
+	Srcs          []string          `mapstructure:"srcs" desc:"Sources for the build"`
+	Outs          []string          `mapstructure:"outs" desc:"Outs for the build"`
 }
 
-func (ncc NpmCiConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
-	return []*zen_targets.Target{
-		zen_targets.NewTarget(
-			ncc.Name,
-			zen_targets.WithSrcs(map[string][]string{"_srcs": ncc.Srcs}),
-			zen_targets.WithOuts([]string{"node_modules"}),
-			zen_targets.WithPassEnv(ncc.PassEnv),
-			zen_targets.WithEnvVars(ncc.Env),
-			zen_targets.WithVisibility(ncc.Visibility),
-			zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
-				Deps: ncc.Deps,
-				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					cmd := exec.Command("npm", "ci")
-					cmd.Dir = target.Cwd
-					cmd.Env = target.GetEnvironmentVariablesList()
-					cmd.Stdout = target
-					cmd.Stderr = target
-					return cmd.Run()
-				},
-			}),
-		),
-	}, nil
+func (ncc NpmCiConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.TargetBuilder, error) {
+	t := zen_targets.ToTarget(ncc)
+	t.Srcs = map[string][]string{"_srcs": ncc.Srcs}
+	t.Outs = []string{"node_modules"}
+	t.Scripts["build"] = &zen_targets.TargetBuilderScript{
+		Deps: ncc.Deps,
+		Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			return target.Exec([]string{"npm", "ci"}, "node ci")
+		},
+	}
+
+	return []*zen_targets.TargetBuilder{t}, nil
 }
